089109a2e1b9b74cc86a86fbd906926880662adb
Nirbhay-sys <119494283+Nirbhay-sys@users.noreply.github.com>
2024-03-30 03:34:15 +0530

    saved the bg color in local storage

--- script.js
+++ script.js
@@ -39,9 +39,25 @@ canvas.addEventListener(
   { passive: false }
 );
 
+const storedTextColor = localStorage.getItem("textColor");
+const storedBackgroundColor = localStorage.getItem("backgroundColor");
+colorPicker.value = storedTextColor || "#000000";
+canvasColor.value = storedBackgroundColor || "#ffffff";
+ctx.strokeStyle = colorPicker.value;
+ctx.fillStyle = colorPicker.value;
+ctx.fillStyle = canvasColor.value;
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
 colorPicker.addEventListener("change", (e) => {
   ctx.strokeStyle = e.target.value;
   ctx.fillStyle = e.target.value;
+  localStorage.setItem("textColor", e.target.value);
+});
+
+canvasColor.addEventListener("change", (e) => {
+  ctx.fillStyle = e.target.value;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  localStorage.setItem("backgroundColor", e.target.value);
 });
 
 canvas.addEventListener("mousedown", (e) => {
@@ -82,15 +98,6 @@ canvas.addEventListener("touchmove", (e) => {
 
 canvas.addEventListener("touchend", () => {
   isDrawing = false;
-});
-
-canvasColor.addEventListener("change", (e) => {
-  ctx.fillStyle = e.target.value;
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-});
-
-fontPicker.addEventListener("change", (e) => {
-  ctx.lineWidth = e.target.value;
 });
 
 clearButton.addEventListener("click", () => {
